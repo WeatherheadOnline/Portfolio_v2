509cf641465b290dc8d64958c21a6c68028beded
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-01-05 11:47:29 -0700

    Added mobile-first styling to skills section

--- photo.js
+++ photo.js
@@ -1,18 +1,12 @@
 const photo = document.getElementById("photo");
 
-// window.onload = function() {
-//     if (scrollY < 100) {
-//         photo.style.visibility = "visible";
-//     }
-// }
-
-window.onscroll = function() {
-    // if (scrollY < 100) {
-    //     photo.style.visibility = "visible";
-    // }
-    if (scrollY > 0) {
-        photo.style.transform = "scaleY(0)";
-    } else if (scrollY == 0) {
-        photo.style.transform = "scaleY(1)";
+if (! ("DeviceOrientation" in document)) {
+    window.onscroll = function() {
+        if (scrollY > 0) {
+            photo.style.transform = "scaleY(0)";
+        } else if (scrollY == 0) {
+            photo.style.transform = "scaleY(1)";
+        }
     }
 }
+
